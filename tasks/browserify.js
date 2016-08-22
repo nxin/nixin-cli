53238224ba2c6f1c90d4bfeeb42b54371e4b9c65
kreo <hi@kreo.me>
2016-08-22 16:56:11 +0200

    removed unused pkgs

--- tasks/browserify.js
+++ tasks/browserify.js
@@ -15,7 +15,6 @@ module.exports = function (gulp, config, utils, $, _) {
         browserify: require("browserify"),
         cached: require("gulp-cached"),
         mirror: require("gulp-mirror"),
-        source: require("vinyl-source-stream"),
         sourcemaps: require("gulp-sourcemaps"),
         uglify: require("gulp-uglify"),
         gzip: require("gulp-gzip"),
@@ -117,45 +116,6 @@ module.exports = function (gulp, config, utils, $, _) {
         });
     }
 
-    // function create() {
-    //     gulp.task("browserify", ["clean:browserify"], function (cb) {
-    //         gulp.src(utils.setSourceStack("browserify", config.browserify.inputExt))
-    //             .pipe($.browserify(config.browserify.opts), function (res) {
-    //                 $.gutil.log("in result");
-    //                 console.log(res);
-    //                 res.on("end", function () {
-    //                     console.log('res.end');
-    //                     cb();
-    //                 });
-    //                 res.on("data", function () {
-    //                     console.log("res.data");
-    //                 });
-    //             }).on("error", function (e) {
-    //             $.gutil.log("in error");
-    //             cb(e);
-    //         })
-    //             .pipe($.cached(config.dest, {
-    //                 extension: '.js'
-    //             }))
-    //             .pipe($.buffer())
-    //             .pipe($.sourcemaps.init({loadMaps: true}))
-    //             .pipe($.rename(function (filepath) {
-    //                 utils.rewritePath(filepath, config.app);
-    //             }))
-    //             .pipe($.if(!process.isProd, $.sourcemaps.write(config.sourcemaps)))
-    //             .pipe($.if(process.isProd, $.mirror(
-    //                 $.uglify(config.browserify.uglify).pipe($.obfuscate()),
-    //                 $.uglify(config.browserify.uglify).pipe($.obfuscate()).pipe($.gzip())
-    //             )))
-    //             .pipe(gulp.dest(config.dest))
-    //             .pipe($.size({
-    //                 showFiles: true
-    //             }))
-    //             .pipe($.browserSync.reload({
-    //                 stream: true
-    //             }));
-    //     });
-    // }
 
     // API
     // ---------------------------------------------------------
