5cc1164c6185dbf0de55615cea0ea0360cde2a6a
kreo <hi@kreo.me>
2016-08-24 11:34:29 +0200

    renamed tests

--- tests/@TODO.md
+++ tests/@TODO.md
@@ -9,13 +9,13 @@ http://stackoverflow.com/questions/31773546/the-best-way-to-run-npm-install-for-
 
 ## @TODO
 ------------------
-1. remove bower
-2. install nested npm/node_modules
-3. install nested gulpfile.js
-4. theme manager
-5. context manager
-6. bundle dist inside context and then copy into public dir
+1. install multiple bower bundles
+2 (no). install nested npm/node_modules
+3 (no). install nested gulpfile.js
+4 (ok). theme manager
+5 (ok). context manager
+6 (ok). bundle dist inside context and then copy into public dir
 7. migrate gulp 3.9.1 => 4.0.0
-8. migrate js => es2015 with babel
+8 (ok). migrate js => es2015 with babel
 9. integrate webpack
-10. integrate node-sass with globbing n indexing
+10 (ok). integrate node-sass with globbing n indexing
---
+++ tests/test-tree-bundles/gulpfile.babel.js
@@ -0,0 +1,70 @@
+/*
+ |--------------------------------------------------------------------------
+ | Nixin-cli
+ |--------------------------------------------------------------------------
+ | author: @kreo
+ | https://github.com/kreo
+ |
+ */
+
+
+/*jshint esversion: 6 */
+"use strict";
+
+import gulp from "gulp";
+import rupture from "rupture";
+import jeet from "jeet";
+import rucksack from "rucksack-css";
+import poststylus from "poststylus";
+
+
+const nix = require("nixin-cli")(gulp, {
+    source: __dirname + "/resources",
+    dest: __dirname + "/public/_dist",
+    app: "app",
+    vendor: "vendor",
+    mail: "mail",
+    npm: {
+        stylus: [
+            rupture(),
+            jeet(),
+            poststylus(rucksack)
+        ],
+        browserify: []
+    },
+    bower: {
+        assets: [
+            "bootstrap"
+        ],
+        order: [
+            "jquery/*",
+            "bootstrap/*",
+            "**/*.js"
+        ]
+    },
+    serve: {
+        host: "localhost",
+        proxy: "localhost/",
+        port: "9001"
+    }
+});
+
+
+nix.run([
+    "default",
+    "images",
+    "fonts",
+    "bower",
+    "stylus",
+    "sass",
+    "less",
+    "browserify"
+]);
+
+
+gulp.task("build", ["bower"], () => {
+    $.runSequence(["browserify", "stylus", "sass", "fonts", "images"]);
+});
+
+
+gulp.task("build:serve", ["build", "serve:watch"]);
