013601639358b6fe2635daabce15bfa162c0aa86
kreo <hi@kreo.me>
2016-08-26 17:06:01 +0200

    fix uglify opts

--- tasks/browserify.js
+++ tasks/browserify.js
@@ -46,20 +46,8 @@ module.exports = (gulp, config, kernel, $) => {
         transform: plugins,
         debug: !process.isProd,
         uglify: {
-            indent_start  : 0,     // start indentation on every line (only when `beautify`)
-            indent_level  : 4,     // indentation level (only when `beautify`)
-            quote_keys    : false, // quote all keys in object literals?
-            space_colon   : true,  // add a space after colon signs?
-            ascii_only    : false, // output ASCII-safe? (encodes Unicode characters as ASCII)
-            inline_script : false, // escape "</script"?
-            width         : 80,    // informative maximum line width (for beautified output)
-            max_line_len  : 32000, // maximum line length (for non-beautified output)
-            ie_proof      : true,  // output IE-safe code?
-            beautify      : false, // beautify output?
-            source_map    : null,  // output a source map
-            bracketize    : true, // use brackets every time?
-            comments      : "license", // output comments?
-            semicolons    : true,  // use semicolons to separate statements? (otherwise, newlines)
+            mangle: true,
+            preserveComments: "license"
         }
     });
 
